c50db6fea20fd84c61fc5b381a8178d9287c0801
Khin22 <k_hinkov@abv.bg>
2019-07-10 20:58:37 +0300

    these too

---
+++ Final Web Project.Data/FinalWebProjectDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Final_Web_Project.DataModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Web_Project.Data
+{
+    public class FinalWebProjectDbContext : IdentityDbContext<FinalWebProjectUser, FinalWebProjectUserRole, string>
+    {
+        public FinalWebProjectDbContext(DbContextOptions<FinalWebProjectDbContext> options) : base(options)
+        {
+
+        }
+        public FinalWebProjectDbContext()
+        {
+
+        }
+    }
+}
---
+++ Final Web Project.Domain/FinalWebProjectUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Final_Web_Project.DataModels
+{
+    public class FinalWebProjectUser : IdentityUser
+    {
+        public FinalWebProjectUser()
+        {
+
+        }
+        public FinalWebProjectUserRole UserRole { get; set; }
+    }
+}
