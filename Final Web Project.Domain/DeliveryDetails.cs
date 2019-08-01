4c11e9b3e889da5826f1ea5a46387582e16b1dce
Khin22 <k_hinkov@abv.bg>
2019-08-01 13:56:10 +0300

    gitIgnore

---
+++ Final Web Project.Domain/DeliveryDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Web_Project.Domain
+{
+    public class DeliveryDetails
+    {
+        public string Id { get; set; }
+
+        public string TwoNames { get; set; }
+
+        public string DeliveryAdderss { get; set; }
+
+        public string City { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
--- InputModels/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace InputModels
-{
-    public class Class1
-    {
-    }
-}
